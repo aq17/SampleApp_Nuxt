b91f912b374325702c26e6206fe7dcef76d6cfca
aq17 <aqneco@gmail.com>
2019-03-31 16:12:56 +0900

    array, tuple

---
+++ Chapter03/array.scala
@@ -0,0 +1,14 @@
+val big = new java.math.BigInteger("12345")
+
+val greetStrings = new Array[String](3)
+greetStrings(0) = "Hello"
+greetStrings(1) = ", "
+greetStrings(2) = "world!\n"
+for (i <- 0 to 2)
+  print(greetStrings(i))
+  
+val numNames = Array("zero", "one", "two")
+val numNames2 = Array.apply("zero", "one", "two")
+
+println(numNames)
+println(numNames2)
